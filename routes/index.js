3f07335b8d9797780368ba40d0d4761455c30566
dajjwwx <dajjwwx@163.com>
2015-02-22 16:42:45 +0800

    删除两个文件

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/users/index',function(req,res,next){
+	
+	var ss = req.query.q + "--" + req.query.shoe.color;
+	
+	var sss = req.param("q") + req.param("shoe").color;
+	
+	res.send("Querying Info:" + ss + "+++" + sss);
+});
+
+router.get('/register',function(req,res,next){
+	res.render('register',{});
+});
+
+router.post('/form',function(req,res){
+	
+	var ss = req.body.username + "--" + req.body.password;
+
+	res.send("Querying Info:" + ss);
+});
+
 module.exports = router;
